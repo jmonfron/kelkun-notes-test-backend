1d6cb438a34ebdee526b507bc66f166636ee8dbe
Jordan <mfrt.jordan@gmail.com>
2025-11-22 14:48:13 +0100

    add task-filters-input dto

---
+++ src/app/project/dto/input/task/task-filters.input.ts
@@ -0,0 +1,17 @@
+import { Field, InputType } from '@nestjs/graphql'
+import { IsOptional, IsUUID } from 'class-validator'
+import { Project } from 'src/database/entities'
+import { EntityExist } from 'src/utils/decorators/entity-exist.validator'
+
+@InputType()
+export class TaskFiltersInput {
+
+  @Field(() => String, {
+    description: `Identifiant du projet`,
+    nullable: true
+  })
+  @IsOptional()
+  @IsUUID('4', { message: `L'identifiant du projet doit être un UUID valide.` })
+  @EntityExist(Project, { message: `Ce projet n'existe pas` })
+  projectId: string
+}
